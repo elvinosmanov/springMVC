4548247313fc628b2f08ff1ca078817eb6543047
elvinosmanov <49588906+elvinosmanov@users.noreply.github.com>
2020-07-04 14:39:57 +0400

    get,update,create methods

---
+++ src/main/java/guru/springframework/services/ProductService.java
@@ -0,0 +1,11 @@
+package guru.springframework.services;
+
+import guru.springframework.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+    Product getProductById(Integer id);
+    Product saveOrUpdateProduct(Product product);
+}
